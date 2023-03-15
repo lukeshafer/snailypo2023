f9db98c738c8a7e0fbfead2ba4008f461fdc0edf
Luke <hello@lukeshafer.com>
2023-03-14 23:35:02 -0400

    add vercel adapter

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,13 @@
 import solid from "solid-start/vite";
-import staticAdapter from "solid-start-static";
+import vercelAdapter from "solid-start-vercel";
 import { defineConfig } from "vite";
 
 export default defineConfig({
 	plugins: [
 		solid({
-			adapter: staticAdapter(),
+			adapter: vercelAdapter({
+				prerender: { expiration: 24 },
+			}),
 		}),
 	],
 });
