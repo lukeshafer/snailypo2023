ba18ccb343e933db63224a07a2dbbcebc39b9b1e
Luke <hello@lukeshafer.com>
2023-03-17 22:13:06 -0400

    improve zine page

--- src/components/Zine.tsx
+++ src/components/Zine.tsx
@@ -1,9 +1,8 @@
-import { createEffect, createMemo, createSignal, createContext, useContext } from "solid-js";
+import { createMemo, createSignal, createContext, useContext, JSX } from "solid-js";
 import type { Zine } from "../config";
 
-const RATIO = 8.5 / 5.5;
+const RATIO = 5.5 / 8.5;
 const WIDTH = 300;
-const HEIGHT = WIDTH * RATIO;
 
 const POSITIONS = [
 	"0% 0%",
@@ -35,14 +34,8 @@ export default function Zine(props: { zine: Zine }) {
 	const rightPage = createMemo(() => currentPage() === LAST_PAGE ? -1 : posIndex())
 	const leftPage = createMemo(() => currentPage() === 0 ? -1 : (posIndex() - 1 + positions().length) % positions().length)
 
-	createEffect(() => {
-		console.log(props.zine.name)
-		console.log("Current page", currentPage());
-		console.log("Right page", rightPage());
-		console.log("Left page", leftPage());
-	})
-
 	function next() {
+		console.log("next");
 		setCurrentPage((page) => {
 			if (page === LAST_PAGE) {
 				return 0;
@@ -51,6 +44,7 @@ export default function Zine(props: { zine: Zine }) {
 	}
 
 	function prev() {
+		console.log("prev");
 		setCurrentPage((page) => {
 			if (page === 0) {
 				return LAST_PAGE;
@@ -61,23 +55,23 @@ export default function Zine(props: { zine: Zine }) {
 	return (
 		<PositionsContext.Provider value={positions}>
 			<h2 class="text-2xl text-center font-bold">{props.zine.name}</h2>
-			<div class="flex gap-4">
-				<Button onClick={prev}>Prev</Button>
-				<Button onClick={next}>Next</Button>
-			</div>
-			<div class="flex">
+			<div class="flex pb-3">
 				<ZinePage
 					src={props.zine.src}
 					position={leftPage()}
 					page={2 * currentPage()}
-					type="left"
-					reversed={props.zine.reversed ?? false} />
+					side="left"
+					reversed={props.zine.reversed ?? false}>
+					<Button onClick={prev} side="left" title="Previous">{'<'}</Button>
+				</ZinePage>
 				<ZinePage
 					src={props.zine.src}
 					position={rightPage()}
 					page={2 * currentPage() + 1}
-					type="right"
-					reversed={props.zine.reversed ?? false} />
+					side="right"
+					reversed={props.zine.reversed ?? false}>
+					<Button onClick={next} side="right" title="Next">{'>'}</Button>
+				</ZinePage>
 			</div>
 		</PositionsContext.Provider>
 	);
@@ -88,40 +82,54 @@ function ZinePage(props: {
 	position: number;
 	page: number;
 	reversed: boolean;
-	type: "left" | "right";
+	side: "left" | "right";
+	children: JSX.Element;
 }) {
 	const positions = useContext(PositionsContext);
 
 	const isRotated = createMemo(() =>
 		props.position >= positions().length / 2
 	);
-	return <div>
-		<div
-			class="flex flex-col items-center justify-center bg-center bg-no-repeat outline outline-2 outline-black/40"
-			style={{
-				"background-image": `url(${props.src})`,
-				width: `${WIDTH}px`,
-				height: `${HEIGHT}px`,
-				"background-size": "400%",
-				"background-position": positions()[Math.abs(props.position)],
-				transform: isRotated() ? "rotate(0.5turn)" : "rotate(0turn)",
-				opacity: props.position === -1 ? 0 : 1,
-			}}
-		/>
-		{
-			props.page > 0 && props.page <= LAST_PAGE * 2 ?
-				<p class="text-center">{props.page}</p>
-				: null
-		}
-	</div>
+	return (
+		<div class="relative">
+			<div
+				class="flex flex-col items-center justify-center bg-center bg-no-repeat outline outline-2 outline-black/40 w-[30vw]"
+				style={{
+					"background-image": `url(${props.src})`,
+					"max-width": `${WIDTH}px`,
+					"aspect-ratio": `${RATIO}`,
+					"background-size": "400%",
+					"background-position": positions()[Math.abs(props.position)],
+					transform: isRotated() ? "rotate(0.5turn)" : "rotate(0turn)",
+					opacity: props.position === -1 ? 0 : 1,
+				}}
+			>
+			</div>
+			{props.children}
+			{
+				props.page > 0 && props.page <= LAST_PAGE * 2 ?
+					<p class="text-center absolute w-full">{props.page}</p>
+					: null
+			}
+		</div>
+	)
 }
 
-function Button(props: { onClick: () => void; children: string }) {
+function Button(props: {
+	onClick: () => void;
+	children: string;
+	side: "left" | "right"
+	title: string;
+}) {
 	return (
 		<button
-			class="bg-purple-500 hover:bg-purple-700 text-white font-bold py-2 px-4 rounded"
+			class="bg-transparent hover:bg-slate-500/50 transition-all text-white font-bold py-2 px-4 rounded absolute h-full right-0 flex items-center top-0"
+			style={{ [props.side]: "0", "justify-content": props.side === "left" ? "flex-start" : "flex-end" }}
 			onClick={() => props.onClick()}>
-			{props.children}
+			<span class="text-white bg-slate-500/25 px-2 rounded text-2xl font-bold" aria-hidden="true">
+				{props.children}
+			</span>
+			<span class="visually-hidden">{props.title}</span>
 		</button>
 	);
 }
